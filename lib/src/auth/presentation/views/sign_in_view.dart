c2aaa32246c1fe5b7e02ade30ed812b89b7df3c3
DevJhones <jhelorsds@gmail.com>
2024-01-02 16:54:56 -0300

    flavor fix

--- lib/src/auth/presentation/views/sign_in_view.dart
+++ lib/src/auth/presentation/views/sign_in_view.dart
@@ -2,7 +2,7 @@ import 'package:demo/core/extensions/context_extension.dart';
 import 'package:demo/core/utils/core_utils.dart';
 import 'package:demo/src/auth/data/models/local_user_model.dart';
 import 'package:demo/src/auth/presentation/bloc/auth_bloc.dart';
-import 'package:demo/src/auth/presentation/views/sign_in_body.dart';
+import 'package:demo/src/auth/presentation/components/sign_in_body.dart';
 import 'package:demo/src/dashboard/presentation/views/dashboard_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
